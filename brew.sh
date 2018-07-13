76f986e2d1e1e1e057ea6f55403d8ad15e302d97
Derek Myers <arcticpro@gmail.com>
2018-07-12 23:12:25 -0500

    Updated brew

--- brew.sh
+++ brew.sh
@@ -16,13 +16,16 @@ brew tap homebrew/versions
 brew tap homebrew/services
 brew tap joshuaclayton/formulae
 
-# Install GNU core utilities (those that come with OS X are outdated)
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
 brew install findutils
 
-# Install Bash 4
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
 brew install bash
 
 # Install more recent versions of some OSX tools
