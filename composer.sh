fa775ec4e1ba580c415a2cd69730ca81a03ad641
Derek Myers <arcticpro@gmail.com>
2019-02-18 13:16:53 -0600

    Updated composer

--- composer.sh
+++ composer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 composer global require "laravel/homestead=~6.2"
-composer global require "laravel/valet=~2.1"
+composer global require "laravel/valet=~2.2"
 composer global require "laravel/installer=~1.4"
 composer global require "laravel/lumen-installer=~1.0"
 composer global require "laravel/envoy=~1.0"
