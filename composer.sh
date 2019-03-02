adc6009218373c101a9005ce6beb3638eb55dcf7
Derek Myers <arcticpro@gmail.com>
2019-03-02 02:15:45 -0600

    Updated composer

--- composer.sh
+++ composer.sh
@@ -2,9 +2,9 @@
 
 composer global require "laravel/homestead=~6.2"
 composer global require "laravel/valet=~2.2"
-composer global require "laravel/installer=~1.4"
-composer global require "laravel/lumen-installer=~1.0"
+composer global require "laravel/installer=^2.0"
+composer global require "laravel/lumen-installer=^1.0"
 composer global require "laravel/envoy=~1.0"
 composer global require "friendsofphp/php-cs-fixer=~1.0"
 composer global require "squizlabs/php_codesniffer=*"
-composer global require "phan/phan"
+composer global require "phan/phan=^1.2"
