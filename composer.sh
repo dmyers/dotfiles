cd0d6c62191ed271e067157681336c525584beaa
Derek Myers <arcticpro@gmail.com>
2019-03-08 13:41:10 -0600

    Updated composer

--- composer.sh
+++ composer.sh
@@ -8,5 +8,6 @@ composer global require "laravel/envoy=~1.0"
 composer global require "friendsofphp/php-cs-fixer=~1.0"
 composer global require "squizlabs/php_codesniffer=*"
 composer global require "phan/phan=^1.2"
-#composer global require "orangehill/iseed"
-#composer global require "xethron/migrations-generator"
+#composer global require orangehill/iseed
+#composer global require xethron/migrations-generator
+composer global require tightenco/tlint
