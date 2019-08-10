410f9070f9102b0ccd2694291f44a94e1acaa550
Derek Myers <arcticpro@gmail.com>
2019-08-10 14:10:48 -0500

    Updated docker

--- docker.sh
+++ docker.sh
@@ -2,3 +2,6 @@
 
 docker pull serjs/go-socks5-proxy
 docker pull kalilinux/kali-linux-docker
+docker pull remnux/metasploit
+docker pull wpscanteam/wpscan
+docker pull wpscanteam/vulnerablewordpress
