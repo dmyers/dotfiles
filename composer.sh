62c3c6b4052199be999b1cd399806ef779583428
Derek Myers <arcticpro@gmail.com>
2019-08-14 00:55:33 -0500

    Updated composer

--- composer.sh
+++ composer.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-composer global require "laravel/homestead=~6.2"
-composer global require "laravel/valet=~2.2"
-composer global require "laravel/installer=^2.0"
-composer global require "laravel/lumen-installer=^1.0"
-composer global require "laravel/envoy=~1.0"
-composer global require "friendsofphp/php-cs-fixer=~1.0"
+composer global require laravel/homestead=~6.2
+composer global require laravel/valet=~2.2
+composer global require laravel/installer=^2.0
+composer global require laravel/lumen-installer=^1.0
+composer global require laravel/envoy=~1.0
+composer global require friendsofphp/php-cs-fixer=~1.0
 composer global require squizlabs/php_codesniffer
-composer global require "phan/phan=^1.2"
+composer global require phan/phan=^1.2
 #composer global require orangehill/iseed
 #composer global require xethron/migrations-generator
 composer global require tightenco/tlint
