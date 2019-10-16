3882e300087db7c38d24c9615b7d64e96d04504c
Derek Myers <arcticpro@gmail.com>
2019-10-16 00:36:06 -0500

    Updated composer

--- composer.sh
+++ composer.sh
@@ -10,10 +10,10 @@ composer global require laravel/installer
 composer global require laravel/spark-installer
 composer global require laravel/lumen-installer
 composer global require laravel/envoy
+composer global require barryvdh/laravel-ide-helper
+composer global require tightenco/tlint
 composer global require friendsofphp/php-cs-fixer
 composer global require squizlabs/php_codesniffer
 composer global require phan/phan
 #composer global require orangehill/iseed
 #composer global require xethron/migrations-generator
-composer global require tightenco/tlint
-composer global require barryvdh/laravel-ide-helper
