6f039b378d45bca90315d00ad9eb9f2b39283a3a
Derek Myers <arcticpro@gmail.com>
2019-10-16 00:37:09 -0500

    Updated composer

--- composer.sh
+++ composer.sh
@@ -12,8 +12,10 @@ composer global require laravel/lumen-installer
 composer global require laravel/envoy
 composer global require barryvdh/laravel-ide-helper
 composer global require tightenco/tlint
-composer global require friendsofphp/php-cs-fixer
-composer global require squizlabs/php_codesniffer
-composer global require phan/phan
 #composer global require orangehill/iseed
 #composer global require xethron/migrations-generator
+
+# Tools
+composer global require phan/phan
+composer global require friendsofphp/php-cs-fixer
+composer global require squizlabs/php_codesniffer
