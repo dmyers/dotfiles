0d280e2479ba883b44f5f33d978c7e4d7bf867af
Derek Myers <arcticpro@gmail.com>
2020-04-15 07:58:05 -0500

    Updated npm

--- npm.sh
+++ npm.sh
@@ -33,6 +33,7 @@ npm install -g eslint-plugin-vue
 npm install -g pug-lint
 
 # Install dev tools
+npm install -g pwa-asset-generator
 npm install -g node-gyp
 npm install -g shelljs
 npm install -g aws-cdk
