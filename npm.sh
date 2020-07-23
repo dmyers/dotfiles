9b344e7981204c70e50a25fb90ccdf74fa30e90f
Derek Myers <arcticpro@gmail.com>
2020-07-23 18:39:47 -0500

    Updated npm

--- npm.sh
+++ npm.sh
@@ -68,6 +68,7 @@ npm install -g hpm-cli
 npm install -g electron
 npm install -g expo-cli
 npm install -g @vue/cli
+npm install -g @vue/cli-service-global
 npm install -g vue-native-cli
 npm install -g vue-language-server
 npm install -g framework7-cli
