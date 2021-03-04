0806db26222082a9020ba1cac69bcbdf71f49d7f
Derek Myers <arcticpro@gmail.com>
2021-03-03 22:11:16 -0600

    Updated cask

--- cask.sh
+++ cask.sh
@@ -90,6 +90,7 @@ brew install --cask gifox
 
 # Install dev apps
 brew install --cask brewservicesmenubar
+brew install --cask devdocs
 brew install --cask phpmon
 brew install --cask aws-vpn-client
 brew install --cask iconizer
