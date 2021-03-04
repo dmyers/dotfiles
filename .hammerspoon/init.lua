dc7c73710be0b6fcd223fedb9a66c3af87837bcd
Derek Myers <arcticpro@gmail.com>
2021-03-03 19:28:13 -0600

    Updated hammerspoon init config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,4 +1,5 @@
 -- @see https://www.hammerspoon.org/docs/hs.application.html
+-- @see https://github.com/Hammerspoon/hammerspoon/wiki/Sample-Configurations
 
 hs.application.enableSpotlightForNameSearches(true)
 
