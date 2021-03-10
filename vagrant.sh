4a46be9e9fc393a5a2e7f4a4a09b813efb02967f
Derek Myers <arcticpro@gmail.com>
2021-03-10 02:49:55 -0600

    Updated vagrant

--- vagrant.sh
+++ vagrant.sh
@@ -2,6 +2,7 @@
 
 vagrant box add ubuntu/bionic64 --provider virtualbox
 vagrant box add laravel/homestead
+
 vagrant plugin install vagrant-vbguest
 vagrant plugin install vagrant-hostsupdater
 vagrant plugin install vagrant-hostmanager
