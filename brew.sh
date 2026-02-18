8c89ec725c494060daf9a088e03f62ae710a04e9
Derek Myers <arcticpro@gmail.com>
2026-02-18 11:49:24 -0600

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -6,9 +6,6 @@ brew update
 # Upgrade any already-installed formulae
 brew upgrade -all
 
-# Tap more package sources
-#brew tap homebrew/versions
-
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
